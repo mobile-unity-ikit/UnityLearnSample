c6fcbbf19b0870a3a2704e0e27b992670e5d51e8
Хорунов Кирилл <kirill.khorounov@yandex.ru>
2023-10-30 14:13:24 +0700

    Update GameObjectActivator

--- Assets/LearnMaterials 2/Scripts/GameObjectActivator.cs
+++ Assets/LearnMaterials 2/Scripts/GameObjectActivator.cs
@@ -7,7 +7,9 @@ using UnityEngine;
 [HelpURL("https://docs.google.com/document/d/1GP4_m0MzOF8L5t5pZxLChu3V_TFIq1czi1oJQ2X5kpU/edit?usp=sharing")]
 public class GameObjectActivator : MonoBehaviour
 {
+    [SerializeField]
     private List<StateContainer> targets;
+    [SerializeField]
     private bool debug;
 
     private void Awake()
@@ -17,10 +19,12 @@ public class GameObjectActivator : MonoBehaviour
             item.defaultValue = item.targetGO.activeSelf;
         }
     }
+    [ContextMenu("Переключить объекты")]
     public void ActivateModule()
     {
         SetStateForAll();
     }
+    [ContextMenu("Переключить объекты в состояние по умолчанию")]
     public void ReturnToDefaultState()
     {
         foreach (var item in targets)
