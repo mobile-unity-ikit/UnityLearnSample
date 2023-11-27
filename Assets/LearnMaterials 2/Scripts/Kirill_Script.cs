7bfcf545d12076221430cd36a75782771ecc37a2
Хорунов Кирилл <kirill.khorounov@yandex.ru>
2023-11-27 15:11:55 +0700

    Kirch_script

--- Assets/LearnMaterials 2/Scripts/Kirill_Script.cs
+++ Assets/LearnMaterials 2/Scripts/Kirill_Script.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
-public class Kirill_Script : MonoBehaviour
+public class Kirill_Script : SampleScript
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private GameObject prefab;
+    [SerializeField]
+    [Range(1, 100)]
+    private int repetitions;
+    [SerializeField]
+    [Range(0.1f, 100)]
+    private float step;
 
-    // Update is called once per frame
-    void Update()
+
+    [ContextMenu("Use")]
+    public override void Use()
     {
-        
+        for (int i = 0; i < repetitions; i++)
+        {
+            Instantiate(prefab, transform.position + i * step * transform.forward, Quaternion.identity);
+        }
     }
 }
