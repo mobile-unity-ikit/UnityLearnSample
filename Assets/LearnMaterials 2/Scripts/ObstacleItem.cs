2acac9200f7d188721fa410ced25f85b95915975
Хорунов Кирилл <kirill.khorounov@yandex.ru>
2023-12-25 09:42:01 +0700

    ObstacleItem

---
+++ Assets/LearnMaterials 2/Scripts/ObstacleItem.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ObstacleItem : MonoBehaviour
+{
+    [SerializeField]
+    private float currentValue = 1f;
+    [SerializeField]
+    private UnityEvent onDestroyObstacle;
+    
+    private Renderer renderer;
+    private Color startColor;
+    private Color endColor;
+
+    private void Start()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+    
+    public void GetDamage(float value)
+    {
+        currentValue -= value;
+        currentValue = Mathf.Clamp(currentValue, 0, 1);
+
+        ChangeColor();
+
+        if (currentValue <= 0)
+        {
+            onDestroyObstacle.Invoke();
+            Destroy(gameObject);
+        }
+    }
+
+    private void ChangeColor()
+    {
+        renderer.material.color = Color.Lerp(endColor, startColor, currentValue);
+    }
+}
